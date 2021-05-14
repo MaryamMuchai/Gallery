33cc45f753fe9fc854bdeb08a3597d251da503e6
Marya <maryammuchai@gmail.com>
2021-05-14 21:51:46 +0300

    creating model category

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.title
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+class Location(models.Model):
+    name = models.CharField(max_length=60)
+    @classmethod
+    def
