1665a7945f7d509a61325b9f6cc44d377f4b8dea
Marya <maryammuchai@gmail.com>
2021-05-14 22:14:36 +0300

    adding the image model

--- gallery/models.py
+++ gallery/models.py
@@ -33,4 +33,21 @@ class Location(models.Model):
 
     def save_location(self):
         self.save()
-        
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = "images/")
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
