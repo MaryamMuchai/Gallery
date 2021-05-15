a9e6503f1d0ccc008bf75a182d4c40963d899fc1
Marya <maryammuchai@gmail.com>
2021-05-15 18:40:24 +0300

    added the view function for the index page

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render, redirect
+from .models import Location, Image, Category
 
 # Create your views here.
+def index(request):
+    images = Image.objects.all()
+    locations = Location.objects.locations()
+    category = Category.objects.all() 
+    return render(request, 'index.html', {'images': images[::-1], 'locations': locations, 'category':category})
